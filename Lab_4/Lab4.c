4042c2673091fab8f3c18645ef091e858a73eeea
ipad2013q <71705996+ipad2013q@users.noreply.github.com>
2020-12-08 23:50:38 +0200

    Update Lab4.c

--- Lab_4/Lab4.c
+++ Lab_4/Lab4.c
@@ -1,25 +1,31 @@
 ﻿#include <stdio.h>
-
+#include <stdbool.h>
 #define M 9
 #define N 8
 
 int matrix[M][N] = {
-        {  22, 19, 16, 12, 9, 6, 3, 0 },
-        {  22, 19, 16, 12, 9, 7, 3, 0 },
-        {  23, 20, 17, 13, 10, 7, 4, 1 },
-        {  23, 20, 17, 14, 10, 8, 4, 1 },
-        {  24, 21, 18, 14, 11, 8, 5, 2 },
-        {  24, 21, 18, 15, 11, 9, 5, 2 },
-        {  25, 22, 19, 15, 12, 9, 6, 3 },
-        {  25, 22, 19, 16, 13, 10, 6, 3 },
-        {  26, 23, 20, 16, 13, 10, 7, 4 } };
-        
-            
- int main(void)
-{
-    int column = 0;
-    printf("Searching for cock\n");
+        { -2, 1, 4, 7, 10, 13, 15, 1 },
+        { -2, 1, 4, 7, 10, 13, 16, 1 },
+        { -2, 1, 4, 7, 10, 13, 16, 1 },
+        { -1, 2, 5, 8, 10, 14, 17, 2 },
+        { -1, 2, 5, 8, 11, 14, 17, 2 },
+        { -1, 2, 5, 8, 12, 14, 17, 2 },
+        {  -1, 3, 6, 9, 12, 15, 18, 2 },
+        {  -1, 3, 6, 9, 12, 15, 18, 2 },
+        {  -1, 3, 6, 9, 12, 15, 18, 2 } };
 
+
+
+int main(void)
+{
+    for (int R = 0; R < M; R++) {
+        for (int C = 0; C < N; C++) {
+            printf("%d ", matrix[R][C]);
+        }
+        printf("\n");
+    }
+    int column = 0;
+    bool flag = true;
     while (column < N)
     {
         int low = 0;
@@ -27,17 +33,18 @@ int matrix[M][N] = {
         while (low <= high)
         {
             int mid = (high + low) / 2;
-            
+
             if (matrix[mid][column] <= 5 && matrix[mid][column] >= 0)
             {
-
-
-                    
-                    printf("Your number on coord [%d, %d]\n", column, mid);
-                    break;
-                
+                printf("\nYour number on coord [%d, %d]\n", mid, column);
+                flag = false;
+                break;
             }
-            else
+            else if (matrix[mid][column] > 5)
+            {
+                high = mid - 1;
+            }
+            else if (matrix[mid][column] < 0)
             {
                 low = mid + 1;
             }
